a685efc1f55686740f09db2c3913468603435397
Aakash Singh <92391710+Aakash231217@users.noreply.github.com>
2025-06-14 12:48:48 +0530

    Update api-proxy.js

--- public/api-proxy.js
+++ public/api-proxy.js
@@ -14,7 +14,7 @@ const PORT = 3001;
 app.use(cors({
   origin: '*',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization','X-Requested-With']
 }));
 app.use(express.json());
